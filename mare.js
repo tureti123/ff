8c29a44a4856382f1d719b7c3adf738dc86f942f
tureti123 <tureti123.troll666@gmail.com>
2024-08-11 19:20:38 +0100

    fsdf

--- mare.js
+++ mare.js
@@ -1,8 +1,36 @@
 import express from "express"
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import mongodb from 'mongodb';
+import path from 'path'
+const { MongoClient } = mongodb;
 const app=express()
 import dotenv from 'dotenv'
 dotenv.config()
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 9000
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const server = createServer(app);
+const io = new Server(server);
+app.use(express.static(path.join(__dirname, 'version2')));
+
+
 app.get('/',(req,res)=>{
-    return res.json({ message:"jfjffjfjf"})
+    res.sendFile(__dirname + "/fin.html");  
+})
+
+const url = 'mongodb+srv://tureti:db7dm8mf@cluster0.tvkiecu.mongodb.net/votreBaseDeDonnées?retryWrites=true&w=majority';
+
+async function connectWithMongoClient() {
+    const client = new MongoClient(url);
+    await client.connect();
+    const database = client.db('votreBaseDeDonnées');
+    collec = database.collection('user');
+}
+app.listen(PORT,()=>{
+    console.log('running')
+})
+io.on('connection', (socket) => {
+    connectWithMongoClient()
 })
